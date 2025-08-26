ebd008f7a6428f297d0f3df87bb358944352d043
Alex84120 <alexandrecastanier84@gmail.com>
2025-08-26 23:24:19 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class", // 🔥 activation du mode sombre
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: "class",
   theme: {
     extend: {},
   },
